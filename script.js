43c76b24288ff80481ceacdc96f6b918534771a9
kramzer22 <markdavidpanganiban1218@hotmail.com>
2023-08-23 19:58:07 +0800

    Add more functionality with menu for mobile

--- script.js
+++ script.js
@@ -4,32 +4,34 @@ const dropMenuSpacer = document.getElementById("dropMenuSpacer");
 
 const menuItem = document.querySelectorAll(".menu-item");
 const returnTop = document.getElementById("returnTop");
+
+var scrollPositionY;
+
 menuSwitch.addEventListener("click", () => {
-  if (dropMenu.style.right === "0") {
-    // dropMenu.style.display = "flex";
+  if (dropMenu.style.right === "0px") {
     dropMenu.style.right = "-100vw";
     returnTop.style.display = "flex";
-
-    console.log("drop menu display flex");
   } else {
-    // dropMenu.style.display = "none";
-    dropMenu.style.right = "0";
+    dropMenu.style.right = "0px";
     returnTop.style.display = "none";
-
-    console.log("drop menu display none");
   }
 });
 
 dropMenuSpacer.addEventListener("click", () => {
   dropMenu.style.right = "-100vw";
   returnTop.style.display = "flex";
-  console.log("drop menu display none");
 });
 
 menuItem.forEach((item) => {
   item.addEventListener("click", () => {
     dropMenu.style.right = "-100vw";
     returnTop.style.display = "flex";
-    console.log("drop menu display none");
   });
+});
+
+window.addEventListener("scroll", () => {
+  if (dropMenu.style.right === "0px") {
+    dropMenu.style.right = "-100vw";
+    returnTop.style.display = "flex";
+  }
 });
